9490fd787f9d256caedecd8ac947b652e1ade894
Koltsun Nikita <koltsun-nv@yandex.ru>
2023-10-10 23:22:24 +0300

    v.0.0.0

---
+++ js/preloader.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const preloader = document.querySelector(".preloder");
+
+preloader.classList.add('active');
+
+setTimeout(()=>{
+    preloader.classList.remove('active');
+}, 500);
+
