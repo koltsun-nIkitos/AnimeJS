6c592e745706cd4925e3a460111b09496bc60fa4
Koltsun Nikita <koltsun-nv@yandex.ru>
2023-10-11 23:13:59 +0300

    v.2.0.0

--- js/bg_elements.js
+++ js/bg_elements.js
@@ -3,10 +3,11 @@ const bgElements = () =>{
 
     const elements = document.querySelectorAll(".set-bg");
 
-    for (let i = 0; i < elements.length; i++) {
-        const src = elements[i].dataset.setbg;
-        elements[i].style.backgroundImage = `url(${src})`;
-    }
+    elements.forEach((element) => {
+        element.style.backgroundImage = `url(${element.dataset.setbg})`;
+    });
+
+
 }
 
 bgElements();
--- js/mainData.js
+++ js/mainData.js
@@ -1,4 +1,29 @@
 const mainData = () => {
+
+    const renderAnimeList = (array, ganres) => {
+        console.log(array, ganres)
+    };
+
+    const renderTopAnime = (array) =>{
+        const wrapper = document.querySelector(".filter__gallery");
+        wrapper.innerHTML = '';
+
+        array.forEach((item) => {
+            wrapper.insertAdjacentHTML('afterbegin', `
+                <div class="product__sidebar__view__item set-bg mix day years"
+                    data-setbg="${item.image}">
+                    <div class="ep">${item.rating} / 10</div>
+                    <div class="view"><i class="fa fa-eye"></i> ${item.views}</div>
+                    <h5><a href="/anime-details.html">${item.title}</a></h5>
+                </div>
+            `)
+        });
+
+        wrapper.querySelectorAll('.set-bg').forEach((element) => {
+            element.style.backgroundImage = `url(${element.dataset.setbg})`;
+        });
+    };
+
     fetch('./db.json')
         .then((response) => {
             if (response.status == 200) {
@@ -14,7 +39,15 @@ const mainData = () => {
             
         })
         .then((data) =>{
-            console.log(data)
+            const ganres = new Set();
+
+            renderTopAnime(data.anime.sort((a, b) =>  b.views - a.views).slice(0, 5));
+
+            data.anime.forEach((item) =>{
+                ganres.add(item.ganre);
+            });
+
+            renderAnimeList(data.anime, ganres);
         })
 };
 
